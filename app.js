59e45c175073004657bf1e37e39a2c273cd093ae
francis coffie <91972297+francis-coffie@users.noreply.github.com>
2022-01-10 14:06:37 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const myAge = 15;
+// this mark the earlyear
+let earlyYears = 2; 
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+// calculating the number of dog years
+ laterYears *= 4;
+// print to console the early and later years
+ console.log(earlyYears);
+ console.log(laterYears);
+ // calculating my age in dog's years
+ let myAgeInDogYears = earlyYears + laterYears;
+// string my name to lowercase 
+ let myName = "francis".toLowerCase();
+ console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
+
+
+
+
